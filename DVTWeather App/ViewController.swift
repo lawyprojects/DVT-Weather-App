40a91cf7334cd0ae2f6809a985f39dc8405b537c
lawyprojects <lawyprojects@gmail.com>
2022-02-23 10:32:12 +0300

    Initial Commit

---
+++ DVTWeather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DVTWeather App
+//
+//  Created by Lawrence Magerer on 23/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
